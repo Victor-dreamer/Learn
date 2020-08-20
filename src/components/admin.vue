82e1d35acca77bf6a06821945e80722dc869c300
Victor-dreamer <Victor_dreamer@163.com>
2020-08-20 14:16:55 +0800

    学习项目初始化

---
+++ src/components/admin.vue
@@ -0,0 +1,26 @@
+<!-- 登录组件 -->
+<template>
+  <div class=''></div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
